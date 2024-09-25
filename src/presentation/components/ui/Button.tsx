98bad0300dbf5bc15a958d5d4bcca3276491386c
Nahuel Lautaro Torres Loretto <112157731+Lautaro261@users.noreply.github.com>
2024-09-25 01:15:58 -0300

    AlertScreen finalizado

--- src/presentation/components/ui/Button.tsx
+++ src/presentation/components/ui/Button.tsx
@@ -18,6 +18,7 @@ export const Button = ({text, styles, onPress}:Props) => {
             opacity: pressed ? 0.8 : 1,
             backgroundColor: colors.primary,
         },
+        styles,
     ])}
     >
       <Text
--- src/presentation/screens/alerts/Alertscreen.tsx
+++ src/presentation/screens/alerts/Alertscreen.tsx
@@ -1,18 +1,96 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { CustomView } from '../../components/ui/CustomView';
+import { Title } from '../../components/ui/Title';
+import { globalStyles } from '../../../config/theme/globalTheme';
+import { Button } from '../../components/ui/Button';
+import { View, Alert } from 'react-native';
+//import prompt from 'react-native-prompt-android';
+import { ShowPrompt } from '../../../config/adapters/Prompt.adapter';
 
 export const Alertscreen = () => {
+
+  const createTwoButtonAlert = () =>
+    Alert.alert('Alert Title', 'My Alert Msg', [
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+      {text: 'OK', onPress: () => console.log('OK Pressed')},
+    ]);
+
+  const createThreeButtonAlert = () =>
+    Alert.alert('Alert Title', 'My Alert Msg', [
+      {
+        text: 'Ask me later',
+        onPress: () => console.log('Ask me later pressed'),
+      },
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+      {text: 'OK', onPress: () => console.log('OK Pressed')},
+    ]);
+
+/*      const ShowPrompt = () =>{
+
+      //Codigo nativo de react native
+       Alert.prompt(
+        '¿Cual es tu correo electronico?',
+        'mensaje de la alerta. loremdefsddfsa',
+        (value:string)=> console.log(value),
+        'secure-text',
+        'Soy el valor por defecto',
+        'number-pad'
+      );
+
+      prompt(
+        'Enter password',
+        'Enter your password to claim your $1.5B in lottery winnings',
+        [
+         {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+         {text: 'OK', onPress: password => console.log('OK Pressed, password: ' + password)},
+        ],
+        {
+            type: 'secure-text',
+            cancelable: false,
+            defaultValue: 'test',
+            placeholder: 'placeholder',
+        }
+    );
+
+
+  };  */
+const onShowPrompt = ()=>{
+  ShowPrompt({
+    title: 'Titulo',
+    subTitle:'subtitulo de la alerta test.',
+    buttons: [
+      {text: 'OK', onPress: ()=>{}},
+    ],
+    placeHolder:'placeholder',
+  });
+};
+
   return (
-    <View style={styles.container}>
-      <Text>Alertscreen</Text>
-    </View>
+    <CustomView style={globalStyles.globalMargin}>
+      <Title safe text="Alertas"/>
+
+      <Button text="Alerta - 2 botones" onPress={createTwoButtonAlert}/>
+
+        <View style={{height: 10}}/>
+
+      <Button text="Alerta - 3 botones" onPress={createThreeButtonAlert}/>
+
+      <View style={{height: 10}}/>
+
+      <Button text="Prompt - Input" onPress={onShowPrompt}/>
+
+      <View style={{height: 10}}/>
+
+    </CustomView>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
